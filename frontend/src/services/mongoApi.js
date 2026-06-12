91933babeb0ecacc0a6e3f31fcb56372e0710eb3
kavya3005 <2500030787@gmail.com>
2026-06-13 00:11:17 +0530

    Fix frontend API URL

--- frontend/src/services/mongoApi.js
+++ frontend/src/services/mongoApi.js
@@ -1,26 +1,34 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api";
+const API =
+  "https://multistep-registration-system.onrender.com/api";
 
+// GET HELP
 export const getHelp = () =>
   axios.get(`${API}/help`);
 
+// GET SESSIONS
 export const getSessions = () =>
   axios.get(`${API}/sessions`);
 
+// GET LOGS
 export const getLogs = () =>
   axios.get(`${API}/logs`);
 
+// SAVE SESSION
 export const saveSession = (data) =>
   axios.post(`${API}/sessions`, data);
 
+// SAVE LOG
 export const saveLog = (data) =>
   axios.post(`${API}/logs`, data);
+
+// SEARCH SESSIONS
 export const searchSessions = (key) =>
-  axios.get(
-    `${API}/search/${key}`
-  );
-  export const getPaginatedSessions = (
+  axios.get(`${API}/search/${key}`);
+
+// PAGINATED SESSIONS
+export const getPaginatedSessions = (
   page,
   size
 ) =>
